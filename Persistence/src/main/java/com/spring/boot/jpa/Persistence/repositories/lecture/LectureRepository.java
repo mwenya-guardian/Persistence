64b028eaa859c585b471859e5cf7650942d97a66
mwenya_guardian <mwenyaguardianmg@gmail.com>
2024-12-15 00:22:36 +0000

    Syntax error fix in LectureRepository

--- Persistence/src/main/java/com/spring/boot/jpa/Persistence/repositories/lecture/LectureRepository.java
+++ Persistence/src/main/java/com/spring/boot/jpa/Persistence/repositories/lecture/LectureRepository.java
@@ -1,6 +1,7 @@
 package com.spring.boot.jpa.Persistence.repositories.lecture;
 
+import com.spring.boot.jpa.Persistence.models.lecture.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LectureRepository extends JpaRepository<LectureRepository, Integer> {
+public interface LectureRepository extends JpaRepository<Lecture, Integer> {
 }
